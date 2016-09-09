db192276c220a447dfb96482690d43ecdd019abc
guider <guider@yeah.net>
2016-09-09 10:04:21 +0800

    n

--- lib/src/main/java/com/yanyuanquan/android/Tip.java
+++ lib/src/main/java/com/yanyuanquan/android/Tip.java
@@ -1,6 +1,7 @@
 package com.yanyuanquan.android;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.SystemClock;
@@ -8,6 +9,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -21,7 +23,7 @@ import com.yanyuanquan.android.lib.R;
  * github https://github.com/guider
  */
 public class Tip {
-
+    String TAG = "Tip";
     private static Tip instance;
     private LinearLayout view;
     private TextView title;
@@ -29,8 +31,10 @@ public class Tip {
 
     private boolean isShowing = false;
 
-    private Tip() {
+    private Runnable runnable;
+    private Handler handler = new Handler(Looper.getMainLooper());
 
+    private Tip() {
     }
 
 
@@ -45,8 +49,13 @@ public class Tip {
         return instance;
     }
 
-    private static Runnable runnable;
-    private static Handler handler = new Handler(Looper.getMainLooper());
+    public void showTiP(android.support.v4.app.Fragment fragment, String message, View.OnClickListener listener) {
+        showTip(fragment.getActivity(), message, listener);
+    }
+
+    public void showTiP(Fragment fragment, String message, View.OnClickListener listener) {
+        showTip(fragment.getActivity(), message, listener);
+    }
 
     public void showTip(Activity activity, String message, View.OnClickListener listener) {
         handler.removeCallbacks(runnable);
@@ -60,7 +69,7 @@ public class Tip {
             view.findViewById(R.id.close).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    // handler对象要一直，否则失效
+                    // handler对象要一致，否则失效
                     handler.removeCallbacks(runnable);
                     remove();
                 }
@@ -77,14 +86,12 @@ public class Tip {
                 remove();
             }
         }, 2000);
-
     }
 
     public void remove() {
         if (view != null && view.getParent() != null) {
             ((ViewGroup) view.getParent()).removeView(view);
         }
-        L("remove ") ;
         isShowing = false;
     }
 
@@ -93,8 +100,17 @@ public class Tip {
     }
 
     private void L(String msg) {
-        Log.e("zjw", msg);
+        Log.e(TAG, msg);
     }
+
+
+
+
+
+
+
+
+
 
 
 }
